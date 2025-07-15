bc1743f54d5f60f9ce741992a305c5a734ad1136
Gyanee01 <gyanendra2301@gmail.com>
2025-07-15 21:25:39 +0530

    new3

--- app.js
+++ app.js
@@ -84,4 +84,6 @@ app.listen(process.env.PORT, () => {
 })
 
 mainModule.children.length.PORT = process.env.PORT || 1900
-on
+on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+});
